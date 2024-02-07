4d9920d305577a7d13685dd53691582aa73a6327
Jochen Stierberger <jochen@stierberger.com>
2024-02-07 13:51:15 -0600

    Add Theme

--- client/src/AppProvider.tsx
+++ client/src/AppProvider.tsx
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { store } from "./core/store";
+import { Theme } from "@radix-ui/themes";
 
 type AppProviderProps = {
   children: React.ReactNode;
@@ -9,7 +10,17 @@ type AppProviderProps = {
 export default function AppProvider({ children }: AppProviderProps) {
   return (
     <Provider store={store}>
-      <Router>{children}</Router>
+      <main className="bg-gray-900 h-screen">
+        <Theme
+          accentColor="jade"
+          grayColor="gray"
+          panelBackground="solid"
+          scaling="100%"
+          radius="full"
+        >
+          <Router>{children}</Router>
+        </Theme>
+      </main>
     </Provider>
   );
 }
